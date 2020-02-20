b39d1d163f1147530e08137f55a0448cfc726cbc
amiradeu <amiradeu.x@gmail.com>
2020-02-21 02:19:18 +0800

    Implement map marker hover
    Using $hover that is automatically pass down

--- src/components/map/map.component.js
+++ src/components/map/map.component.js
@@ -1,48 +1,17 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import axios from "axios";
-import marker from "../../assets/images/marker.png";
-import markerHover from "../../assets/images/marker-hover.png";
+
 import MapMarker from "./marker.component";
 
 const M_SIZE = 40;
 
-const markerStyle = {
-  position: "absolute",
-  width: M_SIZE,
-  height: M_SIZE,
-  left: -M_SIZE / 2, //center coord
-  top: -M_SIZE, // bottom coord
-  cursor: "pointer",
-  backgroundImage: `url("${marker}")`,
-  backgroundSize: "cover"
-};
-
-const markerStyleHover = {
-  position: "absolute",
-  width: M_SIZE,
-  height: M_SIZE,
-  left: -M_SIZE / 2, //center coord
-  top: -M_SIZE, // bottom coord
-  cursor: "pointer",
-  backgroundImage: `url("${markerHover}")`,
-  backgroundSize: "cover"
-};
-
-const markerHeaderStyle = {
-  position: "absolute",
-  width: M_SIZE * 2,
-  height: M_SIZE * 1.4,
-  left: -M_SIZE,
-  bottom: M_SIZE * 2,
-  cursor: "pointer"
-};
 class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      houses: [],
-      styling: markerStyle
+      houses: []
+      // styling: markerStyle
     };
   }
 
@@ -60,19 +29,19 @@ class Map extends Component {
 
   _onChildMouseEnter = (key, childProps) => {
     console.log("mouse enter");
-    const markerId = childProps.house._id;
-    const index = this.state.houses.findIndex(h => h._id === markerId);
-    // if (this.props.onMarkerHover) {
-    //   this.props.onMarkerHover(index);
+    // const markerId = childProps.house._id;
+    // const index = this.state.houses.findIndex(h => h._id === markerId);
+    // if (childProps.house.onMarkerHover) {
+    //   this.childProps.house(index);
     // }
-    console.log(childProps.style);
-    // childProps.style = { markerStyleHover };
-    // this.setState({ styling: markerStyleHover });
   };
 
-  _onChildMouseLeave = () => {
+  _onChildMouseLeave = (key, childProps) => {
     console.log("mouse leave");
-    this.setState({ styling: markerStyle });
+    // this.setState({ styling: markerStyle });
+    // if (childProps.house.onMarkerHover) {
+    //   this.childProps.house(-1);
+    // }
   };
 
   _onChildClick = () => {
@@ -87,7 +56,6 @@ class Map extends Component {
           lat={house.coordinateX}
           lng={house.coordinateY}
           house={house}
-          style={this.state.styling}
         />
       );
     });
@@ -99,9 +67,9 @@ class Map extends Component {
           defaultCenter={[50, 50]}
           defaultZoom={1}
           hoverDistance={M_SIZE / 2}
-          onChildMouseEnter={this._onChildMouseEnter}
-          onChildMouseLeave={this._onChildMouseLeave}
-          onChildClick={this._onChildClick}
+          // onChildMouseEnter={this._onChildMouseEnter}
+          // onChildMouseLeave={this._onChildMouseLeave}
+          // onChildClick={this._onChildClick}
         >
           {MarkerList}
         </GoogleMapReact>
--- src/components/map/marker.component.js
+++ src/components/map/marker.component.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { PropTypes, Component } from "react";
+import marker from "../../assets/images/marker.png";
+import markerHover from "../../assets/images/marker-hover.png";
 
-const MapMarker = ({ house, style }) => {
-  return (
-    <React.Fragment>
-      {/* <div style={markerHeaderStyle}>
+const M_SIZE = 40;
+
+const markerStyle = {
+  position: "absolute",
+  width: M_SIZE,
+  height: M_SIZE,
+  left: -M_SIZE / 2, //center coord
+  top: -M_SIZE, // bottom coord
+  cursor: "pointer",
+  backgroundImage: `url("${marker}")`,
+  backgroundSize: "cover"
+};
+
+const markerStyleHover = {
+  position: "absolute",
+  width: M_SIZE,
+  height: M_SIZE,
+  left: -M_SIZE / 2, //center coord
+  top: -M_SIZE, // bottom coord
+  cursor: "pointer",
+  backgroundImage: `url("${markerHover}")`,
+  backgroundSize: "cover"
+};
+
+const markerHeaderStyle = {
+  position: "absolute",
+  width: M_SIZE * 2,
+  height: M_SIZE * 1.4,
+  left: -M_SIZE,
+  bottom: M_SIZE * 2,
+  cursor: "pointer"
+};
+
+class MapMarker extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const markerState = this.props.$hover ? markerStyleHover : markerStyle;
+    return (
+      <React.Fragment>
+        {/* <div style={markerHeaderStyle}>
           <p>{house.houseName}</p>
           <img
             src={house.houseImg}
@@ -11,9 +52,10 @@ const MapMarker = ({ house, style }) => {
             className="rounded-circle mx-auto d-block"
           ></img>
         </div> */}
-      <div style={style}></div>
-    </React.Fragment>
-  );
-};
+        <div style={markerState}></div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default MapMarker;
