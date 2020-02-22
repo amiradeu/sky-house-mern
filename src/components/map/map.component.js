06188fb2b76b2a194b21fa649173ed36965a77c3
amiradeu <amiradeu.x@gmail.com>
2020-02-22 19:33:48 +0800

    Add 2 different toast view
    One from bootstrap toast.
    Another from pure div code.
    Issue onClick difficulty not solved

--- src/components/map/map.component.js
+++ src/components/map/map.component.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import axios from "axios";
 import MapMarker from "./marker.component";
-import markerPNG from "../../assets/images/marker.png";
-import markerHoverPNG from "../../assets/images/marker-hover.png";
+
 class Map extends Component {
   constructor(props) {
     super(props);
@@ -21,45 +20,6 @@ class Map extends Component {
         });
       })
       .catch(err => console.log(`Error: ${err}`));
-  }
-
-  _onChildMouseEnter = (key, childProps) => {
-    console.log("mouse enter");
-    // const markerId = childProps.house._id;
-    // const index = this.state.houses.findIndex(h => h._id === markerId);
-    // if (childProps.house.onMarkerHover) {
-    //   this.childProps.house(index);
-    // }
-  };
-
-  _onChildMouseLeave = (key, childProps) => {
-    console.log("mouse leave");
-    // this.setState({ styling: markerStyle });
-    // if (childProps.house.onMarkerHover) {
-    //   this.childProps.house(-1);
-    // }
-  };
-
-  _onChildClick = () => {
-    console.log("mouse click");
-  };
-
-  renderMarkers(map, maps) {
-    let infowindow = new maps.InfoWindow({
-      content: "lorem ipsum akdakdn adj ladjl ja kjl jasl"
-    });
-
-    let marker = new maps.Marker({
-      position: { lat: 59.325, lng: 18.07 },
-      map,
-      title: "Hello World!"
-      // icon: { markerPNG }
-      // animation: maps.Animation.DROP
-    });
-
-    marker.addListener("click", function() {
-      infowindow.open(map, marker);
-    });
   }
 
   render() {
@@ -91,15 +51,10 @@ class Map extends Component {
       <div style={{ height: "100vh", width: "100%" }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API }}
-          defaultCenter={[50, 50]}
-          defaultZoom={1}
-          hoverDistance={40}
           options={getMapOptions}
-          onChildMouseEnter={this._onChildMouseEnter}
-          onChildMouseLeave={this._onChildMouseLeave}
-          onChildClick={this._onChildClick}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps }) => this.renderMarkers(map, maps)}
+          defaultCenter={[0, 0]}
+          defaultZoom={3}
+          hoverDistance={40}
         >
           {MarkerList}
         </GoogleMapReact>
--- src/components/map/marker.component.js
+++ src/components/map/marker.component.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal, Button, Toast } from "react-bootstrap";
+import { Toast, Button } from "react-bootstrap";
 import marker from "../../assets/images/marker.png";
 import markerHover from "../../assets/images/marker-hover.png";
 import "./marker.css";
@@ -30,16 +30,78 @@ const markerStyleHover = {
 
 const MapMarker = props => {
   const markerState = props.$hover ? markerStyleHover : markerStyle;
+  const [showInfo, setShowInfo] = useState(0);
   const [show, setShow] = useState(false);
 
   return (
     <React.Fragment>
+      <div
+        style={markerState}
+        onClick={() => {
+          setShowInfo(1);
+          setShow(true);
+          console.log("click to show toast");
+          console.log(`Val: ${showInfo}`);
+        }}
+      />
+      <div className="pulse" onClick={() => setShowInfo(1)}></div>
+      <div
+        className="showInfo"
+        style={{
+          position: "absolute",
+          width: "300px",
+          display: "block",
+          opacity: showInfo,
+          fontSize: ".875rem",
+          backgroundColor: "rgba(255,255,255,.85)",
+          backgroundClip: "padding-box",
+          border: "1px solid rgba(0,0,0,.1)",
+          boxShadow: "0 0.25rem 0.75rem rgba(0,0,0,.1)",
+          borderRadius: ".25rem",
+          backdropFilter: "blur(10px)"
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            padding: ".25rem .75rem",
+            color: "#6c757d",
+            borderBottom: "1px solid rgba(0,0,0,.05)"
+          }}
+        >
+          <img
+            src={props.house.houseImg}
+            style={{ width: "20px", height: "20px" }}
+            className="rounded-circle mr-2"
+          ></img>
+          <strong className="mr-auto">{props.house.houseName}</strong>
+          <small>${props.house.price}</small>
+          <span
+            onClick={() => {
+              setShowInfo(0);
+              console.log("toast close");
+            }}
+            style={{
+              cursor: "pointer",
+              boxSizing: "border-box",
+              fontSize: "1.5rem",
+              fontWeight: "700",
+              lineHeight: "1",
+              textShadow: "0 1px 0 #fff"
+            }}
+          >
+            x
+          </span>
+        </div>
+        <div style={{ padding: ".75rem" }}>{props.house.description}</div>
+      </div>
       <Toast
         style={{ position: "absolute", width: "300px" }}
         onClose={() => setShow(false)}
         show={show}
-        delay={3000}
-        autohide
+        // delay={3000}
+        // autohide
       >
         <Toast.Header>
           <img
@@ -52,8 +114,6 @@ const MapMarker = props => {
         </Toast.Header>
         <Toast.Body>{props.house.description}</Toast.Body>
       </Toast>
-      <div style={markerState} onClick={() => setShow(true)}></div>
-      <div className="pulse" onClick={() => setShow(true)}></div>
     </React.Fragment>
   );
 };
